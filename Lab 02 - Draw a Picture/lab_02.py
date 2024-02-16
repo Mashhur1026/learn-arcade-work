0fbe6e625895877bd15bd3448dbf9b191b7920d1
Mashhur <mashhuryoldoshev2003@gmail.com>
2024-02-16 06:35:43 -0500

    Lab 2 moon and sun

---
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -0,0 +1,54 @@
+import arcade
+
+# Open the window
+arcade.open_window(800, 600, "Different Picture Example")
+
+# Set the background color
+arcade.set_background_color(arcade.color.SKY_BLUE)
+
+# Get ready to draw
+arcade.start_render()
+
+arcade.draw_rectangle_filled(700, 500, 500, 500, arcade.color.DARK_BLUE)
+
+arcade.draw_rectangle_filled(500, 470, 5, 5, arcade.color.WHITE)
+arcade.draw_rectangle_filled(510, 480, 5, 5, arcade.color.WHITE)
+arcade.draw_rectangle_filled(530, 520, 5, 5, arcade.color.WHITE)
+arcade.draw_rectangle_filled(550, 490, 5, 5, arcade.color.WHITE)
+arcade.draw_rectangle_filled(570, 460, 5, 5, arcade.color.WHITE)
+arcade.draw_rectangle_filled(540, 500, 5, 5, arcade.color.WHITE)
+arcade.draw_rectangle_filled(550, 480, 5, 5, arcade.color.WHITE)
+# Draw the moon
+arcade.draw_circle_filled(700, 500, 80, (255, 255, 224))  # RGB for Pale Yellow
+#
+# Draw the mountains
+arcade.draw_triangle_filled(-6000, 100, 800, 400, 900, 0, arcade.color.DARK_GREEN)
+
+# Draw the grass
+arcade.draw_lrtb_rectangle_filled(0, 800, 200, 0, arcade.color.BITTER_LIME)
+
+# Draw a house
+arcade.draw_rectangle_filled(400, 230, 200, 200, arcade.color.BROWN)
+arcade.draw_triangle_filled(300, 330, 500, 330, 400, 480, arcade.color.RED)
+
+
+# Draw the sun
+arcade.draw_circle_filled(100, 500, 50, arcade.color.YELLOW)
+
+# Draw a tree
+arcade.draw_rectangle_filled(600, 100, 20, 100, arcade.color.BROWN)
+arcade.draw_circle_filled(600, 150, 40, arcade.color.DARK_GREEN)
+
+# Draw a tree
+arcade.draw_rectangle_filled(100, 100, 20, 100, arcade.color.BROWN)
+arcade.draw_circle_filled(100, 150, 40, arcade.color.DARK_GREEN)
+
+# Draw a tree
+arcade.draw_rectangle_filled(150, 100, 20, 100, arcade.color.BROWN)
+arcade.draw_circle_filled(150, 150, 40, arcade.color.DARK_GREEN)
+
+# Finish drawing
+arcade.finish_render()
+
+# Keep the window up until someone closes it.
+arcade.run()
---
+++ Sorting Worksheet/drawing_samples.py
@@ -0,0 +1,9 @@
+import arcade
+
+arcade.open_window(600, 600, "Drawing Example")
+
+arcade.set_background_color((175, 0, 42))
+arcade.start_render()
+arcade.finish_render()
+
+arcade.run()
