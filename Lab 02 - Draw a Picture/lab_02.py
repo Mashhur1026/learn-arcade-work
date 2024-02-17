2d9cd7cd0c773280e01759c3bf41d208274c1bd9
Mashhur <mashhuryoldoshev2003@gmail.com>
2024-02-16 20:14:35 -0500

    Lab 2 moon and sun

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -18,9 +18,10 @@ arcade.draw_rectangle_filled(550, 490, 5, 5, arcade.color.WHITE)
 arcade.draw_rectangle_filled(570, 460, 5, 5, arcade.color.WHITE)
 arcade.draw_rectangle_filled(540, 500, 5, 5, arcade.color.WHITE)
 arcade.draw_rectangle_filled(550, 480, 5, 5, arcade.color.WHITE)
+
 # Draw the moon
 arcade.draw_circle_filled(700, 500, 80, (255, 255, 224))  # RGB for Pale Yellow
-#
+
 # Draw the mountains
 arcade.draw_triangle_filled(-6000, 100, 800, 400, 900, 0, arcade.color.DARK_GREEN)
 
@@ -31,9 +32,14 @@ arcade.draw_lrtb_rectangle_filled(0, 800, 200, 0, arcade.color.BITTER_LIME)
 arcade.draw_rectangle_filled(400, 230, 200, 200, arcade.color.BROWN)
 arcade.draw_triangle_filled(300, 330, 500, 330, 400, 480, arcade.color.RED)
 
+# Man
+arcade.draw_circle_filled(700, 150, 20, (0, 0, 0))
+arcade.draw_rectangle_filled(700, 105, 15, 70, (0, 0, 0))
+arcade.draw_rectangle_filled(720, 125, 40, 7, (0, 0, 0))
+arcade.draw_rectangle_filled(680, 125, 40, 7, (0, 0, 0))
 
 # Draw the sun
-arcade.draw_circle_filled(100, 500, 50, arcade.color.YELLOW)
+arcade.draw_arc_filled(100, 370, 150, 150, arcade.color.YELLOW, 0, 180)
 
 # Draw a tree
 arcade.draw_rectangle_filled(600, 100, 20, 100, arcade.color.BROWN)
